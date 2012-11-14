121b9c05eb129f5e8044ab1da4d7db9a113c62f5
Clinton Gormley <develop@traveljury.com>
2012-11-14 12:58:31 +0000

    Import of DRTECH/ElasticSearch-Transport-Curl-0.06 from CPAN.
    gitpan-cpan-distribution: ElasticSearch-Transport-Curl
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         DRTECH/ElasticSearch-Transport-Curl-0.06.tar.gz
    gitpan-cpan-author:       DRTECH
    gitpan-cpan-maturity:     released

--- ElasticSearch-Transport-Curl-0.06/lib/ElasticSearch/Transport/Curl.pm
+++ ElasticSearch-Transport-Curl-0.06/lib/ElasticSearch/Transport/Curl.pm
@@ -7,7 +7,7 @@ use Encode qw(decode_utf8 encode_utf8);
 use ElasticSearch 0.60;
 use parent 'ElasticSearch::Transport';
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 #===================================
 sub protocol     {'http'}
@@ -121,7 +121,7 @@ ElasticSearch::Transport::Curl - A libcurl based HTTP backend for ElasticSearch
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 SYNOPSIS
 
